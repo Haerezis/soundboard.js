f5815a01870dcf6c6c1f554a50186190850385e4
Thomas Kuntz <thomaskuntz67@gmail.com>
2024-06-05 15:00:35 +0200

    Add sound samples

---
+++ src/fixtures/sounds.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 1,
+    name: "Retro Instrument - crystal - C01.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C01.mp3",
+  },
+  {
+    id: 2,
+    name: "Retro Instrument - crystal - C02.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C02.mp3",
+  },
+  {
+    id: 3,
+    name: "Retro Instrument - crystal - C03.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C03.mp3",
+  },
+  {
+    id: 4,
+    name: "Retro Instrument - crystal - C04.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C04.mp3",
+  },
+  {
+    id: 5,
+    name: "Retro Instrument - crystal - C05.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C05.mp3",
+  },
+  {
+    id: 6,
+    name: "Retro Instrument - crystal - C06.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C06.mp3",
+  },
+  {
+    id: 7,
+    name: "Retro Instrument - crystal - C07.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C07.mp3",
+  },
+  {
+    id: 8,
+    name: "Retro Instrument - crystal - C08.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C08.mp3",
+  },
+  {
+    id: 9,
+    name: "Retro Instrument - crystal - C09.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C09.mp3",
+  },
+  {
+    id: 10,
+    name: "Retro Instrument - crystal - C10.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C10.mp3",
+  },
+  {
+    id: 11,
+    name: "Retro Instrument - crystal - C11.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C11.mp3",
+  },
+  {
+    id: 12,
+    name: "Retro Instrument - crystal - C12.mp3",
+    url:
+      "file:///home/haerezis/git/soundboardjs/public/sounds/Retro Instrument - crystal - C12.mp3",
+  },
+];
